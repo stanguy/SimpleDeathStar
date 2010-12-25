4c93c62cda1bc58eadd0b0ca74e66271e8a9f5c4
Sebastien Tanguy <seb@dthg.net>
2010-12-26 00:27:27 +0100

    Initialize

---
+++ Classes/SimpleDeathStarAppDelegate.h
@@ -0,0 +1,32 @@
+//
+//  SimpleDeathStarAppDelegate.h
+//  SimpleDeathStar
+//
+//  Created by Sebastien Tanguy on 12/26/10.
+//  Copyright 2010 dthg.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface SimpleDeathStarAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSString *)applicationDocumentsDirectory;
+- (void)saveContext;
+
+@end
+
