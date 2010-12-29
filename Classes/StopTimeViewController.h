230f74b111243663f7cd2134c77ae7f395201fec
Sebastien Tanguy <seb@dthg.net>
2010-12-29 23:10:17 +0100

    New grid based display of times

--- Classes/StopTimeViewController.h
+++ Classes/StopTimeViewController.h
@@ -10,14 +10,27 @@
 
 @class Line;
 @class Stop;
+@class GridScrollView;
+@protocol GridScrollViewDataSource;
 
-@interface StopTimeViewController : UITableViewController <NSFetchedResultsControllerDelegate> {
+@interface StopTimeViewController : UIViewController <NSFetchedResultsControllerDelegate,UIScrollViewDelegate, GridScrollViewDataSource> {
 @private
     NSFetchedResultsController *fetchedResultsController_;
     Line* line_;
     Stop* stop_;
+    
+    GridScrollView *scrollView;	// holds floating grid
+    UITableView *tableView;
+
 }
 @property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
 @property (nonatomic, retain) Line* line;
 @property (nonatomic, retain) Stop* stop;
+@property (nonatomic, retain) IBOutlet GridScrollView *scrollView;	// holds floating grid
+@property (nonatomic, retain) IBOutlet UITableView *tableView;
+
+- (void)createFloatingGrid;
+- (void)adjustScrollViewFrame;
+- (UIView *)gridScrollView:(GridScrollView *)scrollView tileForRow:(int)row column:(int)column;
+- (void)alignGridAnimated:(BOOL)animated;
 @end
--- Classes/StopTimeViewController.m
+++ Classes/StopTimeViewController.m
@@ -10,24 +10,37 @@
 #import "Line.h"
 #import "Stop.h"
 #import "StopTime.h"
+#import "GridScrollView.h"
 
 #import "SimpleDeathStarAppDelegate.h"
+
+
+const int kRowHeight = 50;
+const int kCellWidth = 44;
+
 
 @implementation StopTimeViewController
 
 @synthesize fetchedResultsController = fetchedResultsController_, line = line_, stop = stop_;
+@synthesize tableView, scrollView;
 
 #pragma mark -
 #pragma mark View lifecycle
 
-/*
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.scrollView.tileWidth  = kCellWidth;
+    self.scrollView.tileHeight = kRowHeight;
+    self.view.clipsToBounds = YES;
+    self.tableView.scrollEnabled = NO;
+    [self createFloatingGrid];
+    
 }
-*/
+
 
 
 #pragma mark -
@@ -35,34 +48,37 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     // Return the number of sections.
-    return [[self.fetchedResultsController sections] count];
+    return 1;
 }
 
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     // Return the number of rows in the section.
-    id <NSFetchedResultsSectionInfo> sectionInfo = [[self.fetchedResultsController sections] objectAtIndex:section];
-    return [sectionInfo numberOfObjects];
+    return [[self.fetchedResultsController sections] count];
 }
 
 
 // Customize the appearance of table view cells.
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
-    static NSString *CellIdentifier = @"Cell";
+    static NSString *CellIdentifier = @"GridCell";
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+        
+        //cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier] autorelease];
+        
+        NSArray *nib = [[NSBundle mainBundle] loadNibNamed:@"GridCell" owner:self options:nil];
+        cell = [nib objectAtIndex:0];
+        
+        cell.accessoryType =  UITableViewCellAccessoryNone; 
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
     }
-    
-    StopTime* st = [self.fetchedResultsController objectAtIndexPath:indexPath];
-    int arrival = [st.arrival intValue] / 60;
-    int mins = arrival % 60;
-    int hours = ( arrival / 60 ) % 24;
-    
-    // Configure the cell...
-    cell.textLabel.text = [NSString stringWithFormat:@"%d:%02d", hours, mins];
+        
+    NSString *stopName = [[[self.fetchedResultsController sections] objectAtIndex:indexPath.row] name];
+    cell.textLabel.font = [UIFont boldSystemFontOfSize:12.0];
+    cell.textLabel.text =  stopName;
+    cell.detailTextLabel.text =  @" ";
     
     return cell;
 }
@@ -80,16 +96,125 @@
 
 #pragma mark -
 #pragma mark Table view delegate
-
-- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section { 
-    return [[[self.fetchedResultsController sections] objectAtIndex:section] name]; 
-}  
-
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return kRowHeight;
+}
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     // Navigation logic may go here. Create and push another view controller.
 }
 
+#pragma mark -
+#pragma mark GridScrollView
+
+- (void)createFloatingGrid {
+    NSLog( @"createFloatingGrid" );
+    self.tableView.hidden = NO;
+    [self.tableView reloadData];
+    self.scrollView.hidden = YES;
+    
+    int nb_max_stops = 0;
+    int nbStops = [[self.fetchedResultsController sections] count];
+    if (nbStops == 0) {
+        return;
+    }
+    for ( int i = 0; i < nbStops ; ++i) {
+        id <NSFetchedResultsSectionInfo> sectionInfo = [[self.fetchedResultsController sections] objectAtIndex:i];
+        if ( [sectionInfo numberOfObjects] > nb_max_stops) {
+            nb_max_stops = [sectionInfo numberOfObjects];
+        }
+    }
+    int gridWidth = (nb_max_stops * kCellWidth) + 12;
+    int gridHeight = (nbStops * kRowHeight);
+    [scrollView setContentSize:CGSizeMake(gridWidth, gridHeight)];
+    
+    [self adjustScrollViewFrame];
+    [self.view bringSubviewToFront:scrollView];
+    
+    //    [self.view addSubview:scrollView];
+    
+    [scrollView reloadData];    
+}
+
+- (void)adjustScrollViewFrame {
+    scrollView.frame = CGRectMake(6, 0, 320, self.view.frame.size.height); 
+    
+}
+
+- (UIView *)gridScrollView:(GridScrollView *)scrollView tileForRow:(int)row column:(int)column {
+    if ( row >= [[self.fetchedResultsController sections] count] ) {
+        return nil;
+    }
+    id <NSFetchedResultsSectionInfo> sectionInfo = [[self.fetchedResultsController sections] objectAtIndex:row];
+    if( column >= [sectionInfo numberOfObjects] ) {
+        return nil;
+    }
+    
+    
+    UIView *cellview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, kCellWidth, kRowHeight)];
+    UILabel *label = [[UILabel alloc] init];
+    label.font = [UIFont systemFontOfSize:11.0];
+    // fill
+    
+    NSIndexPath* indexPath = [NSIndexPath indexPathForRow:column inSection:row];
+    StopTime* st = [self.fetchedResultsController objectAtIndexPath:indexPath];
+    int arrival = [st.arrival intValue] / 60;
+    int mins = arrival % 60;
+    int hours = ( arrival / 60 ) % 24;
+    label.text = [NSString stringWithFormat:@"%02d:%02d", hours, mins];    
+    
+    label.backgroundColor = [UIColor clearColor];
+    
+    if (column % 2 == 0) {
+        cellview.backgroundColor = [UIColor clearColor];
+    } else {
+        cellview.backgroundColor = [UIColor colorWithRed: (214/255.0) green: (214/255.0) blue: (255/255.0) alpha: 0.3];
+    }
+    
+    label.frame = CGRectMake(8, 15, kCellWidth, kRowHeight - 15);
+    [cellview addSubview:label];
+    [label release];
+    
+    
+    return (UIView *)cellview; 
+    
+
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)aScrollView {
+    tableView.contentOffset = CGPointMake(0, aScrollView.contentOffset.y);
+    self.scrollView.directionalLockEnabled = YES; // I don't know why this keeps getting set to NO otherwise
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    [self alignGridAnimated:YES];
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
+    if (!decelerate)
+        [self alignGridAnimated:YES];
+}
+
+- (void)alignGridAnimated:(BOOL)animated {
+    float x = self.scrollView.contentOffset.x;
+    float y = self.scrollView.contentOffset.y;
+    float maxY = self.scrollView.contentSize.height - self.scrollView.frame.size.height - (kRowHeight / 2);
+    
+    if (y == self.scrollView.contentSize.height - self.scrollView.frame.size.height) {
+        return;
+        
+    }
+    // when at bottom, align toward bottom, except when too few rows
+    float newY;
+    if ( (y > maxY ) &&  (maxY >= self.scrollView.frame.size.height) )   {
+        newY = self.scrollView.contentSize.height - self.scrollView.frame.size.height + 10;
+    } else {
+        newY = round(y/kRowHeight) * kRowHeight;
+    }
+    CGPoint contentOffset = CGPointMake( (round(x/kCellWidth) * kCellWidth), newY);
+    
+    [self.scrollView setContentOffset:contentOffset animated:animated];        
+}
 
 #pragma mark -
 #pragma mark Memory management
